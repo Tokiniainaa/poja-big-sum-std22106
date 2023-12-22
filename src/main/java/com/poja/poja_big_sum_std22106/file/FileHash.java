649c893b6279c74fb9687f3a00d9386e409f43b2
tokiniaina <hei.tokiniaina@gmail.com>
2023-12-22 08:23:28 +0300

    generated Poja files

---
+++ src/main/java/com/poja/poja_big_sum_std22106/file/FileHash.java
@@ -0,0 +1,6 @@
+package com.poja.poja_big_sum_std22106.file;
+
+import com.poja.poja_big_sum_std22106.PojaGenerated;
+
+@PojaGenerated
+public record FileHash(FileHashAlgorithm algorithm, String value) {}
---
+++ src/main/java/com/poja/poja_big_sum_std22106/file/FileHashAlgorithm.java
@@ -0,0 +1,9 @@
+package com.poja.poja_big_sum_std22106.file;
+
+import com.poja.poja_big_sum_std22106.PojaGenerated;
+
+@PojaGenerated
+public enum FileHashAlgorithm {
+  SHA256,
+  NONE
+}
